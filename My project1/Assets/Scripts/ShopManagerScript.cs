18fbf20c0fe6eeee6a90c162448b56cb74632506
linh2772001 <linhnguyenhihi2772001@gmail.com>
2023-03-09 12:55:20 +0700

    item trong shop

--- My project1/Assets/Scripts/ShopManagerScript.cs
+++ My project1/Assets/Scripts/ShopManagerScript.cs
@@ -18,6 +18,24 @@ public class ShopManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //ID
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
+        //price
+        shopItems[2,1] = 10;
+        shopItems[2,2] = 20;
+        shopItems[2,3] = 30;
+        shopItems[2,4] = 40;
+        //Quantity
+        shopItems[3, 1] = 0;
+        shopItems[3, 2] = 0;
+        shopItems[3, 3] = 0;
+        shopItems[3, 4] = 0;
+        
+        
+
         
     }
 }
