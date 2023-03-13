6b5d994ba8ba3dd6a6db8c38c4a8304b23c3359e
linh2772001 <linhnguyenhihi2772001@gmail.com>
2023-03-14 00:46:22 +0700

    update skill manager

--- My project1/Assets/Scripts/Skill/SkillManager.cs
+++ My project1/Assets/Scripts/Skill/SkillManager.cs
@@ -1,10 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int priceHealth = 50;
+    public Text text;
+    public HealthHitCharacter healthHitCharacter;
+
+    public void Buyhealth()
+    {
+        if( 0 < healthHitCharacter.hitpoint && healthHitCharacter.hitpoint< healthHitCharacter.Maxhealth)
+        {
+            healthHitCharacter.hitpoint += 5;
+        } else if(healthHitCharacter.hitpoint == healthHitCharacter.Maxhealth)
+        {
+            healthHitCharacter.hitpoint += 0;
+            text.text = "Full of bool";
+        }
+    }
+    public void BuySeep()
+    {
+
+    }
+    public void Buy()
+    {
+
+    }
+
     void Start()
     {
         
