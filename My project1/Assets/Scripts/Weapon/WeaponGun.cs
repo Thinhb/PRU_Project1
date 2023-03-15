95b527ca04487bdc5323251b3ada383d08b4aa98
trunghieu1606 <hieu16062001nk@gmail.com>
2023-03-15 23:01:16 +0700

    update code sung

---
+++ My project1/Assets/Scripts/Weapon/WeaponGun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponGun : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 15;
+
+
+    private void Start()
+    {
+
+
+    }
+    void Update()
+    {
+        Shoot();
+    }
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawnPoint.right * bulletSpeed, ForceMode2D.Impulse);
+
+        }
+
+    }
+}
