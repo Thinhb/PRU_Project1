3e402e28bcec6fb1e402a6479e4dba2d3f7fda68
trunghieu1606 <hieu16062001nk@gmail.com>
2023-03-18 15:39:49 +0700

    commit

--- My project1/Assets/Scripts/Weapon/WeaponGun.cs
+++ My project1/Assets/Scripts/Weapon/WeaponGun.cs
@@ -6,17 +6,37 @@ public class WeaponGun : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public float bulletSpeed = 15;
-
-
-    private void Start()
+    public float bulletSpeed = 10;
+    private float nextActionTime = 1f;
+    public float period = 0.1f;
+    public float updateInterval = 0.5F;
+    private double lastInterval;
+    private int frames;
+    private float fps;
+    void Start()
     {
-
-
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
     }
+
     void Update()
     {
-        autoShoot();
+        
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            autoShoot();
+        }
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = (float)(frames / (timeNow - lastInterval));
+            frames = 0;
+            lastInterval = timeNow;
+
+        } 
+        Debug.Log(fps.ToString("f2"));
     }
     //void Shoot()
     //{
@@ -34,4 +54,5 @@ public class WeaponGun : MonoBehaviour
         var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
         bullet.GetComponent<Rigidbody2D>().AddForce(bulletSpawnPoint.right * bulletSpeed, ForceMode2D.Impulse);
     }
+
 }
