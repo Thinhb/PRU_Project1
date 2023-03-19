1230d5a21ab28a879ab1b3834f44c2cca916324c
linh2772001 <linhnguyenhihi2772001@gmail.com>
2023-03-19 23:25:33 +0700

    su ly ham mua mau

--- My project1/Assets/Scripts/Skill/SkillManager.cs
+++ My project1/Assets/Scripts/Skill/SkillManager.cs
@@ -8,24 +8,35 @@ public class SkillManager : MonoBehaviour
     public int priceHealth = 50;
     public int priceSpeed = 100;
     public int priceSword = 50;
-    public Text text;
+    public Text Displaytext;
     public HealthHitCharacter healthHitCharacter;
     public playerMove playermove;
+    public UI ui;
+    public GameController gameController;
 
     public void Buyhealth()
     {
-        if( 0 < healthHitCharacter.hitpoint && healthHitCharacter.hitpoint< healthHitCharacter.Maxhealth)
+        if( 0 < healthHitCharacter.hitpoint && healthHitCharacter.hitpoint< healthHitCharacter.Maxhealth && gameController.m_score >priceHealth)
         {
             healthHitCharacter.hitpoint += 5;
+            gameController.m_score -= priceHealth;
+            ui.textScore("Coin:" + gameController.m_score);
+            Displaytext.text = "Successfully bought blood!";
         } else if(healthHitCharacter.hitpoint == healthHitCharacter.Maxhealth)
         {
             healthHitCharacter.hitpoint += 0;
-            text.text = "Full of bool";
+            priceHealth = 0;
+            ui.textScore("Coin:" + gameController.m_score);
+            Displaytext.text = "Full of bool!";
+        } else if(gameController.m_score < priceHealth)
+        {
+            Displaytext.text = "Not enough money!!!";
         }
     }
     public void BuySeep()
     {
-        playermove.speedMove += 1;
+      
+
     }
     public void BuySword()
     {
